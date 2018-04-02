66173c3d00c889b51e6a6c8e4e2606e84ae02c8c
Rudi Starcevic <rudi.starcevic@viseo.com>
2018-04-02 16:18:45 +0800

    Remove debug logging

--- src/containers/Auth/auth0-variables.js
+++ src/containers/Auth/auth0-variables.js
@@ -7,8 +7,6 @@ if (process.env.AUTH0_URL) {
   callbackUrl = process.env.AUTH0_URL + '/callback'
 }
 
-console.log(callbackUrl)
-
 export const AUTH_CONFIG = {
   domain: "viseo.auth0.com",
   clientId: "3VXiDHusndIjPa0C9AYCXxSizdbLnHwD",
