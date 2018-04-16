e8161e6c14b7fbc9a180d72b727bec6f05cd41b4
Rudi Starcevic <rudi.starcevic@viseo.com>
2018-04-16 10:26:21 +0800

    Add REACT_APP_AUTH0_URL env var

--- src/config/index.js
+++ src/config/index.js
@@ -3,15 +3,19 @@ import auth0 from "auth0-js";
 // default local development auth callback url
 let callbackUrl = "http://localhost:3000/callback";
 
+if (process.env.REACT_APP_AUTH0_URL) {
+  callbackUrl = process.env.REACT_APP_AUTH0_URL + "/callback";
+}
+
 export const AUTH_CONFIG = {
-    domain: "viseo.auth0.com",
-    clientId: "3VXiDHusndIjPa0C9AYCXxSizdbLnHwD",
-    callbackUrl: callbackUrl
+  domain: "viseo.auth0.com",
+  clientId: "3VXiDHusndIjPa0C9AYCXxSizdbLnHwD",
+  callbackUrl: callbackUrl
 };
 
 const Auth0 = new auth0.WebAuth({
-    domain: AUTH_CONFIG.domain,
-    clientID: AUTH_CONFIG.clientId
+  domain: AUTH_CONFIG.domain,
+  clientID: AUTH_CONFIG.clientId
 });
 
 export default Auth0;
