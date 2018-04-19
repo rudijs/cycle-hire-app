b408d7be02586f5fa5fd5b6f61b243f514777e2b
Rudi Starcevic <rudi.starcevic@viseo.com>
2018-04-19 18:22:49 +0800

    Update Google API Key

--- src/containers/user/Dashboard/components/stateless/MapWithADirectionsRenderer/index.js
+++ src/containers/user/Dashboard/components/stateless/MapWithADirectionsRenderer/index.js
@@ -10,7 +10,7 @@ const {
 const MapWithADirectionsRenderer = compose(
   withProps({
     googleMapURL:
-      "https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places",
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyC_dQy3W2xCy-VYl6HYqap6m3nBV2KL1L0&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />
