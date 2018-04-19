d012ecae812527835d0132490b0241594b9d207a
Rudi Starcevic <rudi.starcevic@viseo.com>
2018-04-19 15:56:09 +0800

    Add User Journey Directions Map

---
+++ src/containers/user/Dashboard/components/stateless/MapWithADirectionsRenderer/index.js
@@ -0,0 +1,53 @@
+import React from "react";
+const { compose, withProps, lifecycle } = require("recompose");
+const {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  DirectionsRenderer
+} = require("react-google-maps");
+
+const MapWithADirectionsRenderer = compose(
+  withProps({
+    googleMapURL:
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />
+  }),
+  withScriptjs,
+  withGoogleMap,
+  lifecycle({
+    componentDidMount() {
+      const DirectionsService = new window.google.maps.DirectionsService();
+
+      DirectionsService.route(
+        {
+          origin: new window.google.maps.LatLng(this.props.beginningStation.lat, this.props.beginningStation.lon),
+          destination: new window.google.maps.LatLng(this.props.dockingStation.lat, this.props.dockingStation.lon),
+          travelMode: window.google.maps.TravelMode.BICYCLING
+        },
+        (result, status) => {
+          if (status === window.google.maps.DirectionsStatus.OK) {
+            this.setState({
+              directions: result
+            });
+          } else {
+            console.error(`error fetching directions ${result}`);
+          }
+        }
+      );
+    }
+  })
+)(props => {
+  return (
+    <GoogleMap
+      defaultZoom={7}
+      defaultCenter={new window.google.maps.LatLng(51.5074, 0.1278)}
+    >
+      {props.directions && <DirectionsRenderer directions={props.directions} />}
+    </GoogleMap>
+  );
+});
+
+export default MapWithADirectionsRenderer;
--- src/containers/user/Dashboard/index.js
+++ src/containers/user/Dashboard/index.js
@@ -1,89 +1,121 @@
-import React, { Component } from 'react';
-import './styles.css';
+import React, { Component } from "react";
+import "./styles.css";
 import GoogleMapHandler from "../../../components/stateful/GoogleMapHandler";
 import DashboardListContainer from "./components/stateless/DashboardList/index";
 import { connect } from "react-redux";
 import { actionSetMapDataSource } from "../../../actions/action-map";
 import ConfirmDialog from "./components/stateless/ConfirmDialog";
+import MapWithADirectionsRenderer from "./components/stateless/MapWithADirectionsRenderer";
+import bikeIcon from "./images/bike-directions-icon.png";
 
 class UserDashboardContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markerToSelect: null,
+      beginningStation: null,
+      dockingStation: null
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            markerToSelect: null,
-            beginningStation: null,
-            dockingStation: null
-        };
+  componentDidMount() {
+    this.getBikepoints();
+  }
+
+  getBikepoints = () => {
+    const { dataSource, actionSetMapDataSource } = this.props;
+    if (!dataSource.items.length) actionSetMapDataSource();
+  };
+
+  footer = dataSource => {
+    if (this.state.markerToSelect == null) {
+      return (
+        <DashboardListContainer
+          type={this.state.beginningStation ? "DOCK" : "BEGIN"}
+          dataSource={dataSource}
+        />
+      );
+    } else {
+      return (
+        <ConfirmDialog
+          type={this.state.beginningStation == null ? "BEGIN" : "DOCK"}
+          station={this.state.markerToSelect}
+          onCancelClick={this._onCancelClick}
+          onSelectClick={this._onSelectClick}
+        />
+      );
     }
+  };
 
-    componentDidMount() {
-        this.getBikepoints();
-    }
+  _onMarkerClick = event => {
+    const { marker } = event;
+    this.setState({ markerToSelect: marker });
+  };
 
-    getBikepoints = () => {
-        const { dataSource, actionSetMapDataSource } = this.props;
-        if(!dataSource.items.length) actionSetMapDataSource();
-    };
+  _onMarkerClusterClick = () => {};
 
-    footer = (dataSource) => {
-        if (this.state.markerToSelect == null) {
-            return (<DashboardListContainer dataSource={dataSource} />)
-        } else {
-            return (
-                <ConfirmDialog
-                    type={this.state.beginningStation == null ? "BEGIN" : "DOCK"}
-                    station={this.state.markerToSelect}
-                    onCancelClick={this._onCancelClick}
-                    onSelectClick={this._onSelectClick}
-                />
-            )
-        }
-    };
+  _onCancelClick = () => {
+    this.setState({ markerToSelect: null });
+  };
 
-    _onMarkerClick = (event) => {
-        const { marker } = event;
-        this.setState({ markerToSelect: marker })
-    };
+  _onSelectClick = () => {
+    const updatedStation =
+      this.state.beginningStation == null
+        ? { beginningStation: this.state.markerToSelect }
+        : { dockingStation: this.state.markerToSelect };
+    this.setState({ ...updatedStation, markerToSelect: null }, () => {
+      console.log(this.state);
+    });
+  };
 
-    _onMarkerClusterClick = () => {
-    };
+  render() {
+    const { dataSource } = this.props;
 
-    _onCancelClick = () => {
-        this.setState({ markerToSelect: null })
-    };
-
-    _onSelectClick = () => {
-        const updatedStation = this.state.beginningStation == null ?
-            { beginningStation: this.state.markerToSelect } :
-            { dockingStation: this.state.markerToSelect };
-        this.setState({ ...updatedStation, markerToSelect: null })
-    };
-
-    render() {
-
-        const { dataSource } = this.props;
-
-        return (
-            <div className="dashboard-container">
-                <GoogleMapHandler
-                    onMarkerClick={this._onMarkerClick}
-                    onMarkerClusterClick={this._onMarkerClusterClick}
-                    dataSource={dataSource}
-                    containerStyle={{ height: "400px" }}
-                />
-                {this.footer(dataSource)}
-            </div>
-        )
-    }
+    return (
+      <div className="dashboard-container">
+        {this.state.beginningStation ? (
+          <p style={{ paddingLeft: 20 }}>
+            <img src={bikeIcon} alt="Bike Icon" />
+            {this.state.beginningStation.commonName}
+          </p>
+        ) : (
+          ""
+        )}
+        {this.state.dockingStation ? (
+          <p style={{ paddingLeft: 20 }}>
+            <img src={bikeIcon} alt="Bike Icon" />
+            {this.state.dockingStation.commonName}
+          </p>
+        ) : (
+          ""
+        )}
+        {this.state.beginningStation && this.state.dockingStation ? (
+          <MapWithADirectionsRenderer
+            beginningStation={this.state.beginningStation}
+            dockingStation={this.state.dockingStation}
+          />
+        ) : (
+          <GoogleMapHandler
+            onMarkerClick={this._onMarkerClick}
+            onMarkerClusterClick={this._onMarkerClusterClick}
+            dataSource={dataSource}
+            containerStyle={{ height: "400px" }}
+          />
+        )}
+        {this.footer(dataSource)}
+      </div>
+    );
+  }
 }
 
 const mapDispatchToProps = dispatch => ({
-    actionSetMapDataSource: ()=> actionSetMapDataSource(dispatch)
+  actionSetMapDataSource: () => actionSetMapDataSource(dispatch)
 });
 
 const mapStateToProps = state => ({
-    dataSource: state.reducerMapDatasource
+  dataSource: state.reducerMapDatasource
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(UserDashboardContainer)
+export default connect(mapStateToProps, mapDispatchToProps)(
+  UserDashboardContainer
+);
